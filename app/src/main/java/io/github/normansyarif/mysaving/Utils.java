95f5a2bf330ac5a1bf60b99c0ac1205e7792f60c
Norman Syarif <normansyarif@icloud.com>
2019-08-02 20:49:26 +0700

    Say goodbye to pressing so many zeros

--- app/src/main/java/io/github/normansyarif/mysaving/Utils.java
+++ app/src/main/java/io/github/normansyarif/mysaving/Utils.java
@@ -2,10 +2,10 @@ package io.github.normansyarif.mysaving;
 
 class Utils {
     static String numberToMoney(int number) {
-        return "Rp. " + String.format("%,d", number);
+        return "Rp. " + String.format("%,d", number * 1000);
     }
 
     static int moneyToNumber(String money) {
-        return Integer.parseInt(money.replace("Rp. ", "").replace(",", "").replace(".", ""));
+        return Integer.parseInt(money.replace("Rp. ", "").replace(",", "").replace(".", "")) / 1000;
     }
 }
